8e12cfb8ca941d1361a9410e3fc3ea54a1005155
Yeampier2022 <yeampierjesusa@gmail.com>
2023-04-18 09:51:03 -0400

    Your Course Assignment Portal Page

---
+++ scripts/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let Country = "Venezuela";
+const name = document.querySelector("#name").textContent = Name;
+
+
+
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+const Year = document.querySelector("#year").textContent = ` ©${year} | ${Name} | ${Country} `
+const Fecha = Document.querySelector("#probando").textContent=`${day}-${month}-${year}`
