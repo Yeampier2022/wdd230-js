13e9f161a26fab3673c1d974b338492c0bbd2865
Yeampier2022 <yeampierjesusa@gmail.com>
2023-04-28 11:25:14 -0400

    add meta description and icon

--- scripts/scripts-prueba.js
+++ scripts/scripts-prueba.js
@@ -4,13 +4,13 @@ let Country = "Venezuela";
 
 
 
-var today = new Date();
-var day = today.getDate();
-var month = today.getMonth() + 1;
-var year = today.getFullYear();
-var hour = today.getHours();
-var min = today.getMinutes()
-var seconds = today.getSeconds()
+const today = new Date();
+const day = today.getDate();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
+const hour = today.getHours();
+const min = today.getMinutes()
+const seconds = today.getSeconds()
 const Year = document.querySelector("#profile").textContent = ` © ${year} | ${Name} | ${Country} `
 const Fecha = document.querySelector("#dates").textContent=`Last Updated: ${day}/${month}/${year} ${year} ${hour}:${min}:${seconds}`
 
