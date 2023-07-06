17e243350b9cf17990128a2972b1a12574e55002
Yeampier2022 <yeampierjesusa@gmail.com>
2023-07-06 17:14:36 -0400

    project weather

--- Project/script/index.js
+++ Project/script/index.js
@@ -7,6 +7,20 @@ function myFunction() {
     }
   }
 
+
+  let Name = "Yeampier H. Fernandez";
+let Country = "Venezuela";
+
+  var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+var hour = today.getHours();
+var min = today.getMinutes()
+var seconds = today.getSeconds()
+const Year = document.querySelector("#profile").textContent = ` © ${year} | Yeampier H. Fernandez | ${Country} `
+const Fecha = document.querySelector("#dates").textContent=`Last Updated: ${day}/${month}/${year} ${year} ${hour}:${min}:${seconds}`
+
   fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
   .then(response => response.json())
   .then(data => console.log(data));
