6345bb767f113a7b3f7affae5186986942a1dcd4
Yeampier2022 <yeampierjesusa@gmail.com>
2023-07-11 14:01:33 -0400

    fresh continue

--- Project/script/fruits.js
+++ Project/script/fruits.js
@@ -1,55 +1,47 @@
+// open the fruits.json file and create a select option for each fruit
+
 document.addEventListener("DOMContentLoaded", () => {
-  // Fetch select
   let selects = document.querySelectorAll("select");
-  // Fetch json data
   fetch("./json/fruits.json").then((data) =>
     data.json().then((data) => {
-      // Create options for every select
       selects.forEach((select) => {
-        // Iterate every fruit
         data.forEach((fruit) => {
-          // Create option
           let option = document.createElement("option");
           option.value = fruit.name;
           option.innerText = fruit.name;
-          // Append to every select
           select.appendChild(option);
         });
       });
     })
   );
 
-  const dateObj = new Date();
-  const lastModified = document.querySelector("#lastModified");
-  const fullYear = document.querySelector("#footer-info p:first-child");
-
-  lastModified.innerText = `Last modified: ${document.lastModified}`;
-
-  const index = fullYear.innerText.indexOf("©");
-  const str = fullYear.innerText
-    .slice(0, index + 1)
-    .concat(dateObj.getFullYear());
-  fullYear.innerText = str.concat(fullYear.innerText.slice(index + 1));
+  // when the form is submitted, create a new element for each input and append it to the result div
 
   document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     let res = document.querySelector("#form-result");
+
     let name = document.createElement("h2");
     name.innerText = document.querySelector("#first-name").value;
     res.append(name);
+
     let email = document.createElement("p");
     email.innerText = `Email: ${document.querySelector("#email").value}`;
     res.append(email);
+
     let number = document.createElement("p");
     number.innerText = `Phone number: ${
       document.querySelector("#phone").value
     }`;
     res.append(number);
+
     let ulHeading = document.createElement("h3");
     ulHeading.innerText = "Selected fruits";
     res.append(ulHeading);
+
     let fruitsList = document.createElement("ul");
     let fruits = [];
+
     selects.forEach((select) => {
       let fruit = document.createElement("li");
       fruit.innerText = select.value;
@@ -64,11 +56,15 @@ document.addEventListener("DOMContentLoaded", () => {
     res.append(instructions);
     const dateObj = new Date();
     let date = document.createElement("p");
-    date.innerText = `Order placed on: ${dateObj.getDate()}/${dateObj.getMonth()}/${dateObj.getFullYear()}`;
+    date.innerText = `Order placed on: ${dateObj.getDate()}/${
+      dateObj.getMonth() + 1
+    }/${dateObj.getFullYear()}`;
+    console.log(date);
     res.append(date);
     let info = document.createElement("ul");
     let infoHeading = document.createElement("h3");
     res.append(infoHeading);
+
     fetch("./json/fruits.json").then((data) =>
       data.json().then((data) => {
         let nutritions = data
